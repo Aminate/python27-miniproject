2da2cf81a0895f9830c444059f8d5988a169c40d
Amina <aminuwko@gmail.com>
2023-03-01 10:58:58 +0600

    shop done

---
+++ shop/manage.py
@@ -0,0 +1,20 @@
+"CRUD"
+# create - создать
+# reade - читать
+# update - обновление
+# delete - удаление
+
+
+
+from products import create, read, delete, update
+
+while True:
+    oper = input("c/r/u/d: ")
+    if oper == 'c':
+        create()
+    elif oper == 'r':
+        read()
+    elif oper == 'u':
+        update()
+    elif oper == 'd':
+        delete()
---
+++ shop/utils.py
@@ -0,0 +1,12 @@
+import json
+
+def get_products()->list:
+    with open("db.json") as file:
+        products = json.load(file)
+        return products
+
+def update_products(products:list):
+    with open("db.json","w") as file:
+        json.dump(products, file)
+
+
